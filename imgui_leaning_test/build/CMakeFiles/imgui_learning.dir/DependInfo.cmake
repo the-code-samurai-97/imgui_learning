404a4ffba9d1a90d471484ab7837f4ad0455c03f
Sibi-Ros-Coder <sibi001@e.ntu.edu.sg>
2023-03-26 20:57:27 +0800

    fixing the bugs for initial working verxion

---
+++ imgui_leaning_test/build/CMakeFiles/imgui_learning.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui_demo.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui_demo.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui_draw.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui_draw.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui_impl_glfw.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_glfw.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui_impl_vulkan.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_vulkan.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_vulkan.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui_tables.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui_tables.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/imgui/imgui_widgets.cpp" "CMakeFiles/imgui_learning.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/imgui/imgui_widgets.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/main.cpp" "CMakeFiles/imgui_learning.dir/main.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/main.cpp.o.d"
+  "/home/sibi/dev/imgui_learning/imgui_leaning_test/yaml_reader.cpp" "CMakeFiles/imgui_learning.dir/yaml_reader.cpp.o" "gcc" "CMakeFiles/imgui_learning.dir/yaml_reader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ imgui_leaning_test/build/CMakeFiles/imgui_learning.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui_learning.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_vulkan.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_impl_vulkan.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/imgui_learning.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/main.cpp.o"
+  "CMakeFiles/imgui_learning.dir/main.cpp.o.d"
+  "CMakeFiles/imgui_learning.dir/yaml_reader.cpp.o"
+  "CMakeFiles/imgui_learning.dir/yaml_reader.cpp.o.d"
+  "imgui_learning"
+  "imgui_learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgui_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
